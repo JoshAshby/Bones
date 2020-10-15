a95b9ea1d25c3d0fe3476093c344a167cc9d4204
JoshAshby <joshuaashby@joshashby.com>
2020-10-15 17:37:56 -0600

    remove repo dir and make tests cleanup

--- test/lib/test_fossil_repo.rb
+++ test/lib/test_fossil_repo.rb
@@ -6,8 +6,12 @@ class TestFossilRepo < Minitest::Test
     @fossil = FossilRepo.new "test"
   end
 
+  def teardown
+    FileUtils.remove_dir FossilRepo.root, true
+  end
+
   def test_that_dir_is_made
-    assert @fossil.root.exist?
+    assert @fossil.repo_root.exist?
   end
 
   def test_that_will_be_skipped
