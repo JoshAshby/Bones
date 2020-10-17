1968f807c6c3b1f12dea81d3da5401338ec64f06
JoshAshby <joshuaashby@joshashby.com>
2020-10-17 12:18:31 -0600

    working on extracting css before going any further

--- app/forms/create_repository.rb
+++ app/forms/create_repository.rb
@@ -5,5 +5,20 @@ module Forms
     def self.from_params params
       new(params.slice(*members.map(&:to_s)))
     end
+
+    def to_forme(**opts)
+      opts[:method] ||= :post
+
+      Forme.form(self, **opts) do |f|
+        f.input :name, type: :text, label: "Repository Name", required: true
+        f.input :password, type: :password, label: "Admin Password"
+
+        f.input :project_name, type: :text, label: "Project Name"
+
+        yield f if block_given?
+
+        f.button "Create Repository"
+      end
+    end
   end
 end
