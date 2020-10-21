4177bc95b8682ecc8f87411af4f808128fa977fb
joshuaashby@joshashby.com <joshuaashby@joshashby.com@noemail.net>
2020-10-21 06:59:21 +0000

    add in simplecov and refactor forme configs to formi namespace and reorg for eager loading
    FossilOrigin-Name: fe2a3faba3eeb1b70124b6ebb6593e51f3e3f8e2e87d49d12a27e6322100a904

---
+++ lib/formi/bones/formatter.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Formi::Bones::Formatter < Forme::Formatter
+  Forme.register_transformer :formatter, :bones, self
+
+  # rubocop:disable Metrics/AbcSize,Metrics/PerceivedComplexity,Lint/AssignmentInCondition
+  def call(input)
+    @input = input
+    @form = input.form
+    attr = input.opts[:attr]
+    @attr = attr ? attr.dup : {}
+    @opts = input.opts
+    normalize_options
+
+    @attr[:class] ||= ""
+
+    case input.type
+    when :button, :submit, :reset
+      Forme.attr_classes(@attr, "a-form__button -primary")
+    else
+      Forme.attr_classes(@attr, "a-form__input")
+    end
+
+    tag = if html = input.opts[:html]
+            html = html.call(input) if html.respond_to?(:call)
+            form.raw(html)
+          else
+            convert_to_tag(input.type)
+          end
+
+    tag = wrap_tag_with_label(tag) if @opts[:label]
+    tag = wrap_tag_with_error(tag) if @opts[:error]
+    tag = wrap(:helper, tag) if input.opts[:help]
+    wrap_tag(tag)
+  end
+  # rubocop:enable Metrics/AbcSize,Metrics/PerceivedComplexity,Lint/AssignmentInCondition
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,21 @@
 
 ENV["RACK_ENV"] = "test"
 
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start do
+    command_name "Minitest::Spec"
+
+    enable_coverage :branch
+    add_filter "/spec/"
+    add_filter "/env.rb"
+
+    %w[ Routes Contracts Forms Cli ].each do |key|
+      add_group key, "app/#{ key.downcase }"
+    end
+  end
+end
+
 require "minitest"
 require "minitest/test"
 require "minitest/spec"
@@ -9,4 +24,5 @@ require "minitest/autorun"
 
 require_relative "../env"
 
+Zeitwerk::Loader.eager_load_all
 LOGGER.remove_handler(:console)
