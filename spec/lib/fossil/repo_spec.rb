06a19e86fb49066e78936e4cc5d7f48c2f64444f
JoshAshby <joshuaashby@joshashby.com>
2020-10-21 16:46:23 -0600

    flesh out some more specs around fossil::repo
    spent a bit of time tracking down some fossil issues and a possible bug in tty-logger
    added a bit of light, not totally helpful docs to fossil::repo as well

--- spec/lib/fossil/repo_spec.rb
+++ spec/lib/fossil/repo_spec.rb
@@ -30,25 +30,51 @@ describe Fossil::Repo do
     end
 
     it "throws a command error when unsuccessful" do
-      process_status_mock = Minitest::Mock.new
-      process_status_mock.expect :exitstatus, 1
-      process_status_mock.expect :success?, false
-
-      subject.stub :run_fossil_command, process_status_mock do
+      stub_failed_command do
         expect { subject.create_repository username: "" }.must_raise Fossil::FossilCommandError
       end
     end
   end
 
+  # Cloning is broken for me with my 2.12.1 version of Fossil from homebrew.
+  # https://fossil-scm.org/forum/forumpost/469343699c?t=h It seems the fix
+  # should be in 2.12 but I'm not 100% sure so I'll wait till 2.13 which is
+  # pending release before trying these again
   describe "cloning repos" do
+    let(:clonable_path) { repo_path.dirname.join("clone-base.fossil") }
+    let(:clonable_repo) { Fossil::Repo.new clonable_path }
+
+    before do
+      skip "Cloning is currently broken on my Fossil 2.12.1 from homebrew"
+      clonable_repo.create_repository username: username
+    end
+
     after do
-      FileUtils.remove_file repo_path
+      FileUtils.remove_file repo_path, true
+      FileUtils.remove_file clonable_path, true
+    end
+
+    it "clones a repo" do
+      expect(subject.clone_repository(url: "file://#{ clonable_path }", username: username)).must_be_nil
+    end
+
+    it "raises a dup repo error" do
+      subject.create_repository username: username
+      expect do
+        subject.clone_repository(url: "file://#{ clonable_path }", username: username)
+      end.must_raise Fossil::ExistingRepositoryError
+    end
+
+    it "raises a command error when failed" do
+      expect { subject.clone_repository(url: "file:///", username: username) }.must_raise Fossil::FossilCommandError
     end
   end
 
-  it "deletes a repo" do
-    expect(subject.create_repository(username: username)).must_be_nil
-    expect(subject.delete_repository!).must_equal 1 # 1 file removed
+  describe "deleting repositories" do
+    it "deletes a repo" do
+      expect(subject.create_repository(username: username)).must_be_nil
+      expect(subject.delete_repository!).must_equal 1 # 1 file removed
+    end
   end
 
   describe "editing a repository" do
@@ -58,6 +84,22 @@ describe Fossil::Repo do
 
     after do
       FileUtils.remove_file repo_path
+    end
+
+    it "creates a user" do
+      password = "testing"
+      expect(subject.create_user(username: "test-1", contact_info: "bones user", password: password))
+        .must_equal password
+    end
+
+    it "creates a user with a random password when not given" do
+      expect(subject.create_user(username: "test-1", contact_info: "bones user")).wont_be_nil
+    end
+
+    it "raises a command error when creating a user fails" do
+      expect do
+        subject.create_user(username: username, contact_info: "bones user")
+      end.must_raise Fossil::FossilCommandError
     end
 
     it "changes the password when given" do
