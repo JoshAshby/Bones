e555d1e99339ce7561d813033d810e9c673147ff
joshuaashby@joshashby.com <joshuaashby@joshashby.com@noemail.net>
2020-10-21 00:23:48 +0000

    start fleshing out specs for fossil::repo
    not 100% sure how to test failing the fossil commands well yet without stubs
    
    FossilOrigin-Name: b2ba113787c23899d94ef658551c2b6d22a39222a2bb0a07abf2d1294f0dbfef

--- lib/fossil/repo.rb
+++ lib/fossil/repo.rb
@@ -39,23 +39,22 @@ class Fossil::Repo
     FileUtils.remove_file repository_file.to_s
   end
 
-  def create_user username:, contact_info:, password:
+  def create_user username:, contact_info:, password: nil
     password ||= SecureRandom.hex(20)
 
     status = run_fossil_command "user", "new", username, contact_info, password, "--repository", repository_file.to_s
     fail "abnormal status creating a new user in fossil #{ self } - #{ status.exitstatus }" unless status.success?
 
-    # Check to see if the user is a setup/superuser. not 100% sure why this is
-    # necessary yet but Flint does it so :shrug:
+    # Ensure the user is a setup/superuser by setting their capabilites
     # https://fossil-scm.org/home/doc/trunk/www/caps/admin-v-setup.md
     status = run_fossil_command "user", "capabilities", username, "s", "--repository", repository_file.to_s
-    fail "new user doesn't have superuser capabilities in fossil #{ self } - #{ status.exitstatus }" unless status.success?
+    fail "abnorla status setting user's superuser capabilities in fossil #{ self } - #{ status.exitstatus }" unless status.success?
 
     password
   end
 
   def change_password username:, password: nil
-    password = SecureRandom.hex(20) if password.blank?
+    password = SecureRandom.hex(20) if password.nil? || password.empty?
 
     status = run_fossil_command "user", "password", username, password, "--repository", repository_file.to_s
     fail "abnormal status creating a new user in fossil #{ self } - #{ status.exitstatus }" unless status.success?
--- spec/lib/fossil/repo_spec.rb
+++ spec/lib/fossil/repo_spec.rb
@@ -1,22 +1,58 @@
 # frozen_string_literal: true
 
 describe Fossil::Repo do
-  after do
-    FileUtils.remove_file repo_path
-  end
-
   let(:repo_path) { Pathname.new(__FILE__).parent.join("test.fossil") }
   subject { Fossil::Repo.new repo_path }
 
   let(:username) { "test-user" }
 
-  it "makes a new repo" do
-    assert 0, subject.create_repository(username: username)
-    assert subject.repository_file.exist?
+  describe "creating repos" do
+    after do
+      FileUtils.remove_file repo_path
+    end
+
+    it "makes a new repo" do
+      expect(subject.create_repository(username: username)).must_be_nil
+      expect subject.repository_file.exist?
+    end
+
+    it "throws making a dup repo" do
+      expect(subject.create_repository(username: username)).must_be_nil
+      expect { subject.create_repository username: username }.must_raise(RuntimeError)
+    end
   end
 
-  it "throws making a dup repo" do
-    assert 0, subject.create_repository(username: username)
-    assert_raises(RuntimeError) { subject.create_repository username: username }
+  describe "cloning repos" do
+    after do
+      FileUtils.remove_file repo_path
+    end
+  end
+
+  it "deletes a repo" do
+    expect(subject.create_repository(username: username)).must_be_nil
+    expect(subject.delete_repository!).must_equal 1 # 1 file removed
+  end
+
+  describe "editing a repository" do
+    before do
+      subject.create_repository username: username
+    end
+
+    after do
+      FileUtils.remove_file repo_path
+    end
+
+    it "changes the password when given" do
+      password = "testing"
+      expect(subject.change_password(username: username, password: password)).must_equal password
+    end
+
+    it "changes the password to a generated one when nil" do
+      expect(subject.change_password(username: username)).wont_be_empty
+    end
+
+    it "changes the password to a generated one when empty" do
+      expect(subject.change_password(username: username, password: "")).wont_equal ""
+    end
   end
 end
