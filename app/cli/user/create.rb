86b17d663e5a94d38db4fcb55b73c2b2a85e5eed
joshashby <joshashby@noemail.net>
2020-10-24 22:27:42 +0000

    refactor of the user create cli class to appease rubocop
    FossilOrigin-Name: b4e2123bf7a26e3e9d30369d7e4d54637e3449ac1cd974c82dd0f3492731726b

--- app/cli/user/create.rb
+++ app/cli/user/create.rb
@@ -17,11 +17,6 @@ class Cli::User::Create < Dry::CLI::Command
 
   private
 
-  def prompt_password
-    print "Password> "
-    @pass = $stdin.gets.strip
-  end
-
   def password
     @pass ||= params[:password]
     @pass ||= SecureRandom.hex(20)
@@ -29,9 +24,14 @@ class Cli::User::Create < Dry::CLI::Command
     @pass
   end
 
-  def run
-    prompt_password if params[:prompt]
+  def prompt_password
+    return unless params[:prompt]
 
+    print "Password> "
+    @pass = $stdin.gets.strip
+  end
+
+  def create_account
     account_id = DB[:accounts].insert(
       email: params[:email],
       username: params[:username],
@@ -42,12 +42,25 @@ class Cli::User::Create < Dry::CLI::Command
       id: account_id,
       password_hash: BCrypt::Password.create(password).to_s
     )
+  end
 
+  def create_fs_data
     Bones::UserFossil.new(params[:username]).ensure_fs!
+  end
 
+  def output_info
     puts "Created user #{ params[:username] }"
     puts "  email: #{ params[:email] }"
     puts "  password: #{ password }" unless params[:password] || params[:prompt]
+  end
+
+  def run
+    prompt_password
+
+    create_account
+    create_fs_data
+
+    output_info
   rescue Sequel::UniqueConstraintViolation => e
     puts "Uh oh, this is embarrassing, something went wrong!"
     puts e.wrapped_exception.message
