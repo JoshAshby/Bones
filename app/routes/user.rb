a8819b1fd57d1a8d9c63835c842b862773500958
joshashby <joshashby@noemail.net>
2020-11-05 01:03:50 +0000

    for lack of a better spot, move the repo class query helper and decorator into app/models
    thinking of moving to dry-view which would help the decorator fit into parts, still unsure where i could
    put the query helper deal though
    
    FossilOrigin-Name: 475b21e4204e257e26cb209aa187d192ce86085bd58e95f3d4754f9825f6015a

--- app/routes/user.rb
+++ app/routes/user.rb
@@ -123,39 +123,3 @@ class Routes::User < Routes::Base
     end
   end
 end
-
-class Repository
-  class RepositoryDecorator < SimpleDelegator
-    attr_accessor :username, :repo, :project_name, :description, :project_code
-
-    def namespaced_name
-      "#{ username }/#{ __getobj__[:name] }"
-    end
-
-    def url
-      "/user/#{ username }/repository/#{ __getobj__[:name] }"
-    end
-  end
-
-  def all_by_account_id id
-    username = DB[:accounts].where(id: id).get(:username)
-    rows = DB[:repositories].where(account_id: id).all
-
-    Bones::UserFossil.new(username).repositories.map do |repo|
-      row = rows.find { _1[:name] == repo.filename }
-
-      dec = RepositoryDecorator.new row
-
-      dec.username = username
-      dec.repo = repo
-
-      repo.repository_db do |db|
-        dec.project_name = db[:config].where(name: "project-name").get(:value)
-        dec.description = db[:config].where(name: "project-description").get(:value)
-        dec.project_code = db[:config].where(name: "project-code").get(:value)
-      end
-
-      dec
-    end
-  end
-end
